6721475a11ff615a371891f37d211f09611cea80
Shubham Kumar <rkshubham108@gmail.com>
2026-01-14 07:18:48 +0530

    Create prog1.py

---
+++ DAY-1/prog1.py
@@ -0,0 +1,13 @@
+a = 10
+b = 20
+c = 30
+print("Variables created.")
+
+print("Current variables:", dir())
+
+del a
+print("After deleting 'p':", dir())
+
+del b
+del c
+print("After deleting all variables:", dir())
