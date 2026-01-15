046d416e5be399d99af74f96f2f620cca5c89cec
Shubham Kumar <rkshubham108@gmail.com>
2026-01-15 07:34:03 +0530

    Create prog2.py

---
+++ DAY-1/prog2.py
@@ -0,0 +1,11 @@
+a = 10              
+b = 3.14             
+name = "Shubham"     
+items = [1, 2, 3]    
+flag = True          
+
+print(type(a))
+print(type(b))
+print(type(name))
+print(type(items))
+print(type(flag))
